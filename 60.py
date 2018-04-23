44d3f44107c0b21a64b417f6c7b0f4a8023ec276
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-04-23 20:59:53 +0200

    solve problem 60

--- 60.py
+++ 60.py
@@ -1,81 +1,89 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2014 Sébastien Diemer <sebastien.diemer@mines-paristech.fr>
+import pickle
+from collections import Counter
+from collections import defaultdict
+from itertools import combinations
+from functools import reduce
 
-"""
 
-"""
+global PRIMES
+global PRIMES_SET
+
+
+def build_primes(N=10**7):
+    primes = [1] * N
+    primes[0] = 0
+    primes[1] = 0
+    for i in range(2, int(N**0.5 + 1)):
+        if primes[i]:
+            j = 2
+            while i * j < N:
+                primes[i*j] = 0
+                j += 1
+    return [i for i, x in enumerate(primes) if x == 1]
+
 
 def is_prime(n):
-    if n == 2 or n == 3: return True
-    i = 2
-    while i*i <= n:
-        if not n%i: return False
-        i += 1
+    primes = PRIMES_SET
+    if n <= 10**8:
+        return n in primes
+    for i in range(2, int(n ** 0.5 + 1)):
+        if not n % i:
+            return False
+    print('found a big prime: ', n)
     return True
 
-assert is_prime(13)
-assert is_prime(17)
-assert not is_prime(28)
 
-def decompose(n):
-    s = str(n)
-    return [(int(s[:i]), int(s[i:])) for i in xrange(1, len(s)) if s[i] != '0']
-
-assert decompose(7109) == [(7, 109), (710, 9)]
+def is_prime_pair(x, y):
+    return is_prime(int('%s%s' % (x, y))) and is_prime(int('%s%s' % (y, x)))
 
 
-PRIMES = {}
-N = 300000
-for n in xrange(2, N):
-    if not n in PRIMES:
-        if is_prime(n):
-            PRIMES[n] = tuple()
-            for p, q in decompose(n):
-                if p in PRIMES and q in PRIMES:
-                    m = int('%d%d' % (q, p))
-                    if m < n:
-                        if m in PRIMES:
-                            # p, q can be put together to form two new primes
-                            # we discovered one new 'match' for each:
-                            PRIMES[p] += (q,)
-                            PRIMES[q] += (p,)
-                    elif is_prime(m):
-                        PRIMES[m] = tuple()
-                        # p, q can be put together to form two new primes
-                        # we discovered one new 'match' for each:
-                        PRIMES[p] += (q,)
-                        PRIMES[q] += (p,)
-                
-from itertools import combinations
+def find_primes_pairs():
+    """find pairs of two primes that concatenate into a prime."""
+    subset = PRIMES[:1100]  # this value needed some tweaking
+    primes = PRIMES_SET
+    return set(
+        (x, y)
+        for x, y in combinations(subset, 2)
+        if x != y and is_prime_pair(x, y)
+    )
 
-def test_five(five_primes):
-    for e1, e2 in combinations(five_primes, 2):
-        if e1[0] in e2[1] and e2[0] in e1[1]:
-            pass
-        else:
-            i1, i2 = int('%d%d' % (e1[0], e2[0])), int('%d%d' % (e2[0], e1[0]))
-            if i1 > N and i2 > N:
-                if is_prime(i1) and is_prime(i2):
-                    pass
-                else:
-                    return False
-            else: return False
-    return True
 
-test = [p for p in PRIMES[3] if p in PRIMES[7]]
-print test
-for t1, t2 in combinations(test, 2):
-    i1, i2 = int('%d%d' % (t1, t2)), int('%d%d' % (t2, t1))
-    if is_prime(i1) and is_prime(i2):
-        print t1, t2
-print is_prime(int('%d%d' % (9901, 673)))
-print is_prime(int('%d%d' % (673, 9901)))
-#for el in combinations([(p, q) for p, q in PRIMES.items() if len(q) > 2], 5):
-#    print el[1]
-    #if test_five(el):
-    #    print [e[0] for e in el]
-    #    break
-            
+def find_remarkable_primes(primes_pairs, size=4):
+    primes_with_prime_pair = defaultdict(list)
+    for pair in primes_pairs:
+        primes_with_prime_pair[pair[0]].append(pair[1])
+        primes_with_prime_pair[pair[1]].append(pair[0])
+
+    candidates = {
+        x: set(y)
+        for x, y in primes_with_prime_pair.items()
+        if len(set(y)) >= size - 1
+    }
+
+    ret = []
+    for x in candidates:
+        for y in candidates[x]:
+            if y in candidates:
+                if len((set(candidates[y]) | set([y])) & (candidates[x] | set([x]))) >= size:
+                    ret.append(tuple(sorted(list((set(candidates[y]) | set([y])) & (candidates[x] | set([x]))))))
+
+    return min(
+        (sum(x), x)
+        for x, y in Counter(ret).items()
+        if y >= size and all(is_prime_pair(u, v) for u, v in combinations(x, 2))
+    )
+
+if __name__ == '__main__':
+    global PRIMES, PRIMES_SET
+    with open('PRIMES_10_8', 'r') as f:
+        PRIMES = list(int(x) for x in f.read().split('\n') if x)
+        PRIMES_SET = set(PRIMES)
+
+    assert is_prime_pair(3, 7)
+    assert is_prime_pair(3, 109)
+    assert is_prime_pair(3, 673)
+    assert is_prime_pair(7, 109)
+    assert is_prime_pair(7, 673)
+    assert is_prime_pair(109, 673)
+    print('checks done')
+    print(find_remarkable_primes(find_primes_pairs(), size=5))
